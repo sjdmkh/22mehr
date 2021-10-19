7226dba04ec0317eb2cf539186f8c23344db88fd
Sajjad Mahmoudi Khah <sajadmahmoodikhah@gmail.com>
2021-10-19 16:40:43 +0330

    practice 0 has been modified

---
+++ js/p0.js
@@ -0,0 +1,8 @@
+function star(n){
+    for (let i=1; i <= n; i++){
+        if ( i===1 || i===n) console.log('*'.repeat(n))
+        else console.log('*'+' '.repeat(n-2)+'*')
+    }
+}
+console.log(star(6))
+console.log(star(4))
