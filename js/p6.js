ed19a3a7c28a68b45826048bf91aa9b23eaf9da2
Sajjad Mahmoudi Khah <sajadmahmoodikhah@gmail.com>
2021-10-20 17:00:44 +0330

    practice 6 has been done

---
+++ js/p6.js
@@ -0,0 +1,11 @@
+function neighbourhood (str){
+    let result = '';
+    for (let i = 0; i < str.length; i++){
+        if ((str[i]) !== (str[i+1]))
+            result += str[i];
+    }
+    return result;
+}
+
+console.log(neighbourhood('aahaaabbbbkkkcccddddeeefff'))
+console.log(neighbourhood('aajaaabblbbkkkcccddrddeeefff'))
